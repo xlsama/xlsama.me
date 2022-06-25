d034a101344bea10a756b88eb2e446098affb18d
xlsama <im.xlsama@gmail.com>
2022-06-25 21:58:14 +0800

    chore: update

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,6 +4,9 @@ import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import css from './index.module.scss'
 
+import path from 'path'
+import { readFileSync } from 'fs'
+
 type Props = {
   data: string
 }
@@ -15,19 +18,17 @@ const About: NextPage<Props> = ({ data }) => {
         <title>关于我</title>
       </Head>
       <Header />
-      <main className={css.about}>
-        <Markdown>{data}</Markdown>
-      </main>
+      <main className={css.about}>{data && <Markdown>{data}</Markdown>}</main>
     </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async context => {
-  const res = await fetch('http://localhost:3000/api/post/about')
-  const data = await res.json()
+  const filePath = path.resolve('_posts', 'about.md')
+  const data = readFileSync(filePath).toString()
 
   return {
-    props: data,
+    props: { data },
   }
 }
 
